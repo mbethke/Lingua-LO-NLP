f24d95751117ff5a5cb72b2e02bb4386cd50883f
Matthias Bethke <matthias.bethke@financial.com>
2016-09-06 12:11:24 +0700

    nicer formatting

--- lib/Lingua/LO/Transform/Regexp.pm
+++ lib/Lingua/LO/Transform/Regexp.pm
@@ -10,90 +10,90 @@ use charnames qw/ :full lao /;
 # Regular expression fragments. The cryptic names correspond to the naming
 # in PHISSAMAY at al: Syllabification of Lao Script for Line Breaking
 my %regexp_fragments = (
-    x0_1 => 'ເ',
-    x0_2 => 'ແ',
-    x0_3 => 'ໂ',
-    x0_4 => 'ໄ',
-    x0_5 => 'ໃ',
+    x0_1    => 'ເ',
+    x0_2    => 'ແ',
+    x0_3    => 'ໂ',
+    x0_4    => 'ໄ',
+    x0_5    => 'ໃ',
 
-    x1 => 'ຫ',
+    x1      => 'ຫ',
 
-    x => '[ກຂຄງຈສຊຍດຕຖທນບປຜຝພຟມຢຣລວຫອຮໜໝ]',
+    x       => '[ກຂຄງຈສຊຍດຕຖທນບປຜຝພຟມຢຣລວຫອຮໜໝ]',
 
-    x2 => "[\N{LAO SEMIVOWEL SIGN LO}ຣວລ]",
+    x2      => "[\N{LAO SEMIVOWEL SIGN LO}ຣວລ]",
 
-    x3 => "[\N{LAO VOWEL SIGN U}\N{LAO VOWEL SIGN UU}]",
+    x3      => "[\N{LAO VOWEL SIGN U}\N{LAO VOWEL SIGN UU}]",
 
-    x4_12 => "[\N{LAO VOWEL SIGN I}\N{LAO VOWEL SIGN II}]",
-    x4_34 => "[\N{LAO VOWEL SIGN Y}\N{LAO VOWEL SIGN YY}]",
-    x4_5  => "\N{LAO NIGGAHITA}",
-    x4_6  => "\N{LAO VOWEL SIGN MAI KAN}",
-    x4_7  => "\N{LAO VOWEL SIGN MAI KON}",
-    x4_1t4 => "[\N{LAO VOWEL SIGN I}\N{LAO VOWEL SIGN II}\N{LAO VOWEL SIGN Y}\N{LAO VOWEL SIGN YY}]",
+    x4_12   => "[\N{LAO VOWEL SIGN I}\N{LAO VOWEL SIGN II}]",
+    x4_34   => "[\N{LAO VOWEL SIGN Y}\N{LAO VOWEL SIGN YY}]",
+    x4_5    => "\N{LAO NIGGAHITA}",
+    x4_6    => "\N{LAO VOWEL SIGN MAI KAN}",
+    x4_7    => "\N{LAO VOWEL SIGN MAI KON}",
+    x4_1t4  => "[\N{LAO VOWEL SIGN I}\N{LAO VOWEL SIGN II}\N{LAO VOWEL SIGN Y}\N{LAO VOWEL SIGN YY}]",
 
-    x5 => "[\N{LAO TONE MAI EK}\N{LAO TONE MAI THO}\N{LAO TONE MAI TI}\N{LAO TONE MAI CATAWA}]",
+    x5      => "[\N{LAO TONE MAI EK}\N{LAO TONE MAI THO}\N{LAO TONE MAI TI}\N{LAO TONE MAI CATAWA}]",
 
-    x6_1 => 'ວ',
-    x6_2 => 'ອ',
-    x6_3 => 'ຽ',
-    x6 => '[ວອຽ]',
+    x6_1    => 'ວ',
+    x6_2    => 'ອ',
+    x6_3    => 'ຽ',
+    x6      => '[ວອຽ]',
 
-    x7_1 => 'ະ',
-    x7_2 => 'າ',
-    x7_3 => "\N{LAO VOWEL SIGN AM}",
+    x7_1    => 'ະ',
+    x7_2    => 'າ',
+    x7_3    => "\N{LAO VOWEL SIGN AM}",
 
-    x8_3t8 => '[ຍດນມຢບ]',
-    x8 => '[ກງຍດນມຢບວ]',
+    x8_3t8  => '[ຍດນມຢບ]',
+    x8      => '[ກງຍດນມຢບວ]',
 
-    x9 => '[ຈສຊພຟລ]',
+    x9      => '[ຈສຊພຟລ]',
 
-    x10_12=> '[ຯໆ]',
-    x10_3 => "\N{LAO CANCELLATION MARK}",
+    x10_12  => '[ຯໆ]',
+    x10_3   => "\N{LAO CANCELLATION MARK}",
 
     x9a10_3 => '(?: $x9 $x10_3)',
 
     re1_all => '$x0_1 $x1? $x $x2?',
-    re1_1 => '$x5? $x8? $x9a10_3?',
-    re1_2 => '$x4_12 $x5? $x8? $x9a10_3?',
-    re1_3 => '$x4_34 $x5? $x6_2 $x8? $x9a10_3?',
-    re1_4 => '$x7_2 $x7_1',
-    re1_5 => '$x4_6 $x5? $x7_2',
-    re1_6 => '$x4_7 $x5? $x8  $x9a10_3?',
-    re1_8 => '$x4_7? $x5? $x6_3',
+    re1_1   => '$x5? $x8? $x9a10_3?',
+    re1_2   => '$x4_12 $x5? $x8? $x9a10_3?',
+    re1_3   => '$x4_34 $x5? $x6_2 $x8? $x9a10_3?',
+    re1_4   => '$x7_2 $x7_1',
+    re1_5   => '$x4_6 $x5? $x7_2',
+    re1_6   => '$x4_7 $x5? $x8  $x9a10_3?',
+    re1_8   => '$x4_7? $x5? $x6_3',
 
     re2_all => '$x0_2 $x1? $x $x2?',
-    re2_1 => '$x5? $x6? $x8? $x9a10_3?',
-    re2_2 => '$x7_1',
-    re2_3 => '$x4_7 $x5? $x8 $x9a10_3?',
+    re2_1   => '$x5? $x6? $x8? $x9a10_3?',
+    re2_2   => '$x7_1',
+    re2_3   => '$x4_7 $x5? $x8 $x9a10_3?',
 
     re3_all => '$x0_3 $x1? $x $x2?',
-    re3_1 => '$x5? $x8? $x9a10_3?',
-    re3_2 => '$x7_1',
-    re3_3 => '$x4_7 $x5? $x8_3t8?',
+    re3_1   => '$x5? $x8? $x9a10_3?',
+    re3_2   => '$x7_1',
+    re3_3   => '$x4_7 $x5? $x8_3t8?',
 
-    re4 => '$x0_4 $x1? $x $x2? $x5? $x6_1? $x9a10_3?',
+    re4     => '$x0_4 $x1? $x $x2? $x5? $x6_1? $x9a10_3?',
 
-    re5 => '$x0_5 $x1? $x $x2? $x5? $x6_1?',
+    re5     => '$x0_5 $x1? $x $x2? $x5? $x6_1?',
 
-    re6 => '$x1? $x $x2? $x3 $x5? $x8? $x9a10_3?',
+    re6     => '$x1? $x $x2? $x3 $x5? $x8? $x9a10_3?',
 
-    re7 => '$x1? $x $x2? $x4_1t4 $x5? $x8? $x9a10_3?',
+    re7     => '$x1? $x $x2? $x4_1t4 $x5? $x8? $x9a10_3?',
 
-    re8 => '$x1? $x $x2? $x4_5 $x5? $x7_2? $x9a10_3?',
+    re8     => '$x1? $x $x2? $x4_5 $x5? $x7_2? $x9a10_3?',
 
-    re9 => '$x1? $x $x2? $x4_6 $x5? (?: $x8 $x9a10_3? | $x6_1 $x7_1 )',
+    re9     => '$x1? $x $x2? $x4_6 $x5? (?: $x8 $x9a10_3? | $x6_1 $x7_1 )',
 
-    re10=> '$x1? $x $x2? $x4_7 $x5? $x6_1 $x8 $x9a10_3?',
+    re10    => '$x1? $x $x2? $x4_7 $x5? $x6_1 $x8 $x9a10_3?',
 
-    re11=> '$x1? $x $x2? $x5? $x6 $x8 $x9a10_3?',
+    re11    => '$x1? $x $x2? $x5? $x6 $x8 $x9a10_3?',
 
-    re12=> '$x1? $x $x2? $x5? $x7_1',
+    re12    => '$x1? $x $x2? $x5? $x7_1',
 
-    re13=> '$x1? $x $x2? $x5? $x7_2 $x8? $x9a10_3?',
+    re13    => '$x1? $x $x2? $x5? $x7_2 $x8? $x9a10_3?',
 
-    re14=> '$x1? $x $x2? $x5? $x7_3 $x9a10_3?',
+    re14    => '$x1? $x $x2? $x5? $x7_3 $x9a10_3?',
 
-    re_num => '[໑໒໓໔໕໖໗໘໙໐]',
+    re_num  => '[໑໒໓໔໕໖໗໘໙໐]',
 );
 
 # Functional names for all the x-something groups from the original paper
@@ -120,7 +120,7 @@ my @sorted_x_names = ('x9a10_3', reverse sort { length $a <=> length $b } keys %
 # There are two regexp building blocks that need to be listed first
 my @fragment_subst_names = ('x9a10_3', sort grep { /^re/ } keys %regexp_fragments);
 
-sub assemble_regexp {
+sub _assemble_regexp {
     my $fragments = shift;
     our %e;
     *e = $fragments;
@@ -144,7 +144,7 @@ sub syllable_short {
         }
     }
 
-    my $syl = assemble_regexp(\%fragments);
+    my $syl = _assemble_regexp(\%fragments);
     return qr/ $syl /x;
 }
 
@@ -157,7 +157,7 @@ sub syllable_named {
         }
     }
 
-    my $syl = assemble_regexp(\%fragments);
+    my $syl = _assemble_regexp(\%fragments);
     return qr/ $syl /x;
 }
 
