0ca37fe171f454732e54f2713cc43e94c30749ca
Matthias Bethke <matthias.bethke@financial.com>
2016-09-16 08:20:11 +0700

    simplify: do regexp fragment subst on finished RE

--- lib/Lingua/LO/Transform/Regexp.pm
+++ lib/Lingua/LO/Transform/Regexp.pm
@@ -51,50 +51,63 @@ my %regexp_fragments = (
     x10_3   => "\N{LAO CANCELLATION MARK}",
 
     x9a10_3 => '(?: $x9 $x10_3)',
-
-    re1_all => '$x0_1 $x1? $x $x2?',
-    re1_1   => '$x5? $x8? $x9a10_3?',
-    re1_2   => '$x4_12 $x5? $x8? $x9a10_3?',
-    re1_3   => '$x4_34 $x5? $x6_2 $x8? $x9a10_3?',
-    re1_4   => '$x7_2? $x7_1',
-    re1_5   => '$x4_6 $x5? $x7_2',
-    re1_6   => '$x4_7 $x5? $x8  $x9a10_3?',
-    re1_8   => '$x4_7? $x5? $x6_3',
-
-    re2_all => '$x0_2 $x1? $x $x2?',
-    re2_1   => '$x5? $x6? $x8? $x9a10_3?',
-    re2_2   => '$x7_1',
-    re2_3   => '$x4_7 $x5? $x8 $x9a10_3?',
-
-    re3_all => '$x0_3 $x1? $x $x2?',
-    re3_1   => '$x5? $x8? $x9a10_3?',
-    re3_2   => '$x7_1',
-    re3_3   => '$x4_7 $x5? $x8_3t8?',
-
-    re4     => '$x0_4 $x1? $x $x2? $x5? $x6_1? $x9a10_3?',
-
-    re5     => '$x0_5 $x1? $x $x2? $x5? $x6_1?',
-
-    re6     => '$x1? $x $x2? $x3 $x5? $x8? $x9a10_3?',
-
-    re7     => '$x1? $x $x2? $x4_1t4 $x5? $x8? $x9a10_3?',
-
-    re8     => '$x1? $x $x2? $x4_5 $x5? $x7_2? $x9a10_3?',
-
-    re9     => '$x1? $x $x2? $x4_6 $x5? (?: $x8 $x9a10_3? | $x6_1 $x7_1 )',
-
-    re10    => '$x1? $x $x2? $x4_7 $x5? $x6_1? $x8 $x9a10_3?',
-
-    re11    => '$x1? $x $x2? $x5? $x6 $x8 $x9a10_3?',
-
-    re12    => '$x1? $x $x2? $x5? $x7_1',
-
-    re13    => '$x1? $x $x2? $x5? $x7_2 $x8? $x9a10_3?',
-
-    re14    => '$x1? $x $x2? $x5? $x7_3 $x9a10_3?',
-
-    re_num  => '[໑໒໓໔໕໖໗໘໙໐]',
 );
+my $re1_all = '$x0_1 $x1? $x $x2?';
+my $re1_1   = '$x5? $x8? $x9a10_3?';
+my $re1_2   = '$x4_12 $x5? $x8? $x9a10_3?';
+my $re1_3   = '$x4_34 $x5? $x6_2 $x8? $x9a10_3?';
+my $re1_4   = '$x7_2? $x7_1';
+my $re1_5   = '$x4_6 $x5? $x7_2';
+my $re1_6   = '$x4_7 $x5? $x8  $x9a10_3?';
+my $re1_8   = '$x4_7? $x5? $x6_3';
+
+my $re2_all = '$x0_2 $x1? $x $x2?';
+my $re2_1   = '$x5? $x6? $x8? $x9a10_3?';
+my $re2_2   = '$x7_1';
+my $re2_3   = '$x4_7 $x5? $x8 $x9a10_3?';
+
+my $re3_all = '$x0_3 $x1? $x $x2?';
+my $re3_1   = '$x5? $x8? $x9a10_3?';
+my $re3_2   = '$x7_1';
+my $re3_3   = '$x4_7 $x5? $x8_3t8?';
+
+my $re4     = '$x0_4 $x1? $x $x2? $x5? $x6_1? $x9a10_3?';
+
+my $re5     = '$x0_5 $x1? $x $x2? $x5? $x6_1?';
+
+my $re6     = '$x1? $x $x2? $x3 $x5? $x8? $x9a10_3?';
+
+my $re7     = '$x1? $x $x2? $x4_1t4 $x5? $x8? $x9a10_3?';
+
+my $re8     = '$x1? $x $x2? $x4_5 $x5? $x7_2? $x9a10_3?';
+
+my $re9     = '$x1? $x $x2? $x4_6 $x5? (?: $x8 $x9a10_3? | $x6_1 $x7_1 )';
+
+my $re10    = '$x1? $x $x2? $x4_7 $x5? $x6_1? $x8 $x9a10_3?';
+
+my $re11    = '$x1? $x $x2? $x5? $x6 $x8 $x9a10_3?';
+
+my $re12    = '$x1? $x $x2? $x5? $x7_1';
+
+my $re13    = '$x1? $x $x2? $x5? $x7_2 $x8? $x9a10_3?';
+
+my $re14    = '$x1? $x $x2? $x5? $x7_3 $x9a10_3?';
+
+my $re_num  = '[໑໒໓໔໕໖໗໘໙໐]';
+
+my $rex1012 = '$x10_12';
+
+my $re_basic = <<EOF;
+(?: (?:
+(?: $re1_all (?: $re1_1 | $re1_2 | $re1_3 | $re1_4 | $re1_5 | $re1_6 | $re1_8 ) ) |
+(?: $re2_all (?: $re2_1 | $re2_2 | $re2_3 ) ) |
+(?: $re3_all (?: $re3_1 | $re3_2 | $re3_3 ) ) |
+$re4  | $re5  | $re6  | $re7  | $re8  | $re9  | $re10 |
+$re11 | $re12 | $re13 | $re14 ) $rex1012? | $re_num+ )
+EOF
+$re_basic =~ s/\n//gs;
+$re_basic =~ s/\s+/ /g; # keep it a bit more readable. could use s/\s+//g
+
 # Functional names for all the x-something groups from the original paper
 my %capture_names = (
     'x'             => 'consonant',
@@ -114,50 +127,29 @@ my %capture_names = (
     'x10_3'         => 'cancel',
 );
 # Substitute longer fragment names first so their matches don't get swallowed
-# by the shorter ones.
+# by the shorter ones. x9a10_3 is a convenience shotcut for '(?: $x9 $x10_3)'
+# so we have to do it first.
 my @sorted_x_names = ('x9a10_3', reverse sort { length $a <=> length $b } keys %capture_names);
-# There are two regexp building blocks that need to be listed first
-my @fragment_subst_names = ('x9a10_3', sort grep { /^re/ } keys %regexp_fragments);
-
-sub _assemble_regexp {
-    my $fragments = shift;
-    our %e;
-    *e = $fragments;
-
-    return "(?: (?:
-    (?: $e{re1_all} (?: $e{re1_1} | $e{re1_2} | $e{re1_3} | $e{re1_4} | $e{re1_5} | $e{re1_6} | $e{re1_8} ) ) |
-    (?: $e{re2_all} (?: $e{re2_1} | $e{re2_2} | $e{re2_3} ) ) |
-    (?: $e{re3_all} (?: $e{re3_1} | $e{re3_2} | $e{re3_3} ) ) |
-    $e{re4}  | $e{re5}  | $e{re6}  | $e{re7}  | $e{re8}  | $e{re9}  | $e{re10} |
-    $e{re11} | $e{re12} | $e{re13} | $e{re14} ) $e{x10_12}? | $e{re_num}+ )";
-}
 
 sub syllable_short {
     my %fragments = %regexp_fragments;  # full copy as we'll be modifying it
 
-    # Just do some manual scalar interpolation. This could be done at compile time
-    # but we need the flexibility for inserting named captures below
-    for my $frag (@fragment_subst_names) {
-        for my $atom (@sorted_x_names) {
-            $fragments{$frag} =~ s/\$($atom)/$fragments{$1}/eg;
-        }
+    my $syl_re = $re_basic;
+    for my $atom (@sorted_x_names) {
+        $syl_re =~ s/\$($atom)/$fragments{$1}/eg;
     }
 
-    my $syl = _assemble_regexp(\%fragments);
-    $ENV{DEBUG} and say $syl;
-    return qr/ $syl /x;
+    return = qr/ $syl_re /x;
 }
 
 sub syllable_named {
     my %fragments = %regexp_fragments;  # full copy as we'll be modifying it
 
-    for my $frag (@fragment_subst_names) {
-        for my $atom (@sorted_x_names) {
-            $fragments{$frag} =~ s/\$($atom)/_named_capture(\%fragments, $atom, $1)/eg;
-        }
+    my $syl_capture = $re_basic;
+    for my $atom (@sorted_x_names) {
+        $syl_capture =~ s/\$($atom)/_named_capture(\%fragments, $atom, $1)/eg;
     }
 
-    my $syl_capture = _assemble_regexp(\%fragments);
     my $syl_short = syllable_short();
     return qr/ $syl_capture (?= \P{Lao} | \s+ | $ | $syl_short )/x;
 }
